e8a83288979a2f92c4af7109c5d765dfefd7e77b
Imad Archid <imadarchid@gmail.com>
2016-02-01 13:10:11 +0000

    Fix Safari/Firefox bug.

--- public/class-voip-down-maroc-public.php
+++ public/class-voip-down-maroc-public.php
@@ -96,7 +96,7 @@ class Voip_Down_Maroc_Public {
 		 * class.
 		 */
 
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/voip-down-maroc-public.js', array( 'jquery' ), $this->version, false );
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/voip-down-maroc-public.js', array( 'jquery' ), $this->version, true );
 
 	}
 
